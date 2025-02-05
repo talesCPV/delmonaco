0279d8aec63d6110a41adbe56cb408cf9e103a54
talesCPV <tales@flexibus.com.br>
2025-02-05 17:03:34 -0300

    by_script

---
+++ sql/delmonaco_sp.sql
@@ -0,0 +1,290 @@
+/* FUNCTIONS */
+
+ DROP PROCEDURE IF EXISTS sp_getHash;
+DELIMITER $$
+	CREATE PROCEDURE sp_getHash(
+		IN Iemail varchar(80),
+		IN Isenha varchar(30)
+    )
+	BEGIN    
+		SELECT SHA2(CONCAT(Iemail, Isenha), 256) AS HASH;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_allow;
+DELIMITER $$
+	CREATE PROCEDURE sp_allow(
+		IN Iallow varchar(80),
+		IN Ihash varchar(64)
+    )
+	BEGIN    
+		SET @access = (SELECT IFNULL(access,-1) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		SET @quer =CONCAT('SET @allow = (SELECT ',@access,' IN ',Iallow,');');
+			PREPARE stmt1 FROM @quer;
+			EXECUTE stmt1;
+	END $$
+DELIMITER ;
+
+/* LOGIN */
+
+ DROP PROCEDURE IF EXISTS sp_login;
+DELIMITER $$
+	CREATE PROCEDURE sp_login(
+		IN Iemail varchar(80),
+		IN Isenha varchar(30)
+    )
+	BEGIN    
+		SET @hash = (SELECT SHA2(CONCAT(Iemail, Isenha), 256));
+		SELECT *, IF(nome="",SUBSTRING_INDEX(email,"@",1),nome) AS nome FROM tb_usuario WHERE hash=@hash;
+	END $$
+DELIMITER ;
+
+/* USER */
+
+ DROP PROCEDURE IF EXISTS sp_newUser;
+DELIMITER $$
+	CREATE PROCEDURE sp_newUser(
+        IN Inome varchar(30),
+		IN Iemail varchar(80),
+		IN Isenha varchar(30),
+        IN Iasaas_id varchar(16)
+    )
+	BEGIN
+		SET @has_user = (SELECT COUNT(*) FROM tb_usuario WHERE email COLLATE utf8_general_ci = Iemail COLLATE utf8_general_ci);
+		IF (@has_user = 0)THEN
+			SET @hash = SHA2(CONCAT(Iemail, Isenha), 256);
+			INSERT INTO tb_usuario (nome,email,hash,asaas_id)VALUES(Inome,Iemail,@hash,Iasaas_id);
+			CALL sp_add_credit(Iasaas_id,3,0);
+			SELECT hash FROM tb_usuario WHERE id= (SELECT MAX(id) FROM tb_usuario);
+        ELSE
+			SELECT 0 AS hash;
+        END IF;
+	END $$
+DELIMITER ;
+
+
+ DROP PROCEDURE IF EXISTS sp_viewUser;
+DELIMITER $$
+	CREATE PROCEDURE sp_viewUser(
+		IN Iallow varchar(80),
+		IN Ihash varchar(64),
+		IN Ifield varchar(30),
+        IN Isignal varchar(4),
+		IN Ivalue varchar(50)
+    )
+	BEGIN    
+		CALL sp_allow(Iallow,Ihash);
+		IF(@allow)THEN
+			SET @quer =CONCAT('SELECT id,email,access, IF(access=0,"ROOT",IFNULL((SELECT nome FROM tb_usr_perm_perfil WHERE USR.access = id),"DESCONHECIDO")) AS perfil FROM tb_usuario AS USR WHERE ',Ifield,' ',Isignal,' ',Ivalue,' ORDER BY ',Ifield,';');
+			PREPARE stmt1 FROM @quer;
+			EXECUTE stmt1;
+		ELSE 
+			SELECT 0 AS id, "" AS email, 0 AS access;
+        END IF;
+	END $$
+DELIMITER ;
+
+/* PERMISSÂO */
+
+ DROP PROCEDURE IF EXISTS sp_set_usr_perm_perf;
+DELIMITER $$
+	CREATE PROCEDURE sp_set_usr_perm_perf(	
+		IN Iallow varchar(80),
+		IN Ihash varchar(64),
+        In Iid int(11),
+		IN Inome varchar(30)
+    )
+	BEGIN    
+		CALL sp_allow(Iallow,Ihash);
+		IF(@allow)THEN   
+			IF(Iid = 0 AND Inome != "")THEN
+				INSERT INTO tb_usr_perm_perfil (nome) VALUES (Inome);
+            ELSE
+				IF(Inome = "")THEN
+					DELETE FROM tb_usr_perm_perfil WHERE id=Iid;
+				ELSE
+					UPDATE tb_usr_perm_perfil SET nome = Inome WHERE id=Iid;
+                END IF;
+            END IF;			
+			SELECT * FROM tb_usr_perm_perfil;
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_view_usr_perm_perf;
+DELIMITER $$
+	CREATE PROCEDURE sp_view_usr_perm_perf(	
+		IN Iallow varchar(80),
+		IN Ihash varchar(64),
+		IN Ifield varchar(30),
+        IN Isignal varchar(4),
+		IN Ivalue varchar(50)
+    )
+	BEGIN
+		CALL sp_allow(Iallow,Ihash);
+		IF(@allow)THEN   
+			SET @quer = CONCAT('SELECT * FROM tb_usr_perm_perfil WHERE ',Ifield,' ',Isignal,' ',Ivalue,' ORDER BY ',Ifield,';');
+			PREPARE stmt1 FROM @quer;
+			EXECUTE stmt1;
+		ELSE 
+			SELECT 0 AS id, "" AS nome;
+        END IF;
+	END $$
+DELIMITER ;
+
+/* CALENDAR */
+
+ DROP PROCEDURE IF EXISTS sp_view_calendar;
+DELIMITER $$
+	CREATE PROCEDURE sp_view_calendar(	
+		IN Ihash varchar(64),
+		IN IdataIni date,
+		IN IdataFin date
+    )
+	BEGIN    
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		SELECT * FROM tb_calendario WHERE id_user=@id_call AND data_agd>=IdataIni AND data_agd<=IdataFin;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_set_calendar;
+DELIMITER $$
+	CREATE PROCEDURE sp_set_calendar(	
+		IN Ihash varchar(64),
+		IN Idata date,
+		IN Iobs varchar(255)
+    )
+	BEGIN    
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+        IF(@id_call >0)THEN
+			SET @exist = (SELECT COUNT(*) FROM tb_calendario WHERE id_user=@id_call AND data_agd = Idata);
+			IF(@exist AND Iobs = "")THEN
+				DELETE FROM tb_calendario WHERE id_user=@id_call AND data_agd = Idata; 
+			ELSE
+				INSERT INTO tb_calendario (id_user, data_agd, obs) VALUES(@id_call, Idata, Iobs)
+                ON DUPLICATE KEY UPDATE obs=Iobs;
+			END IF;
+        END IF;
+	END $$
+DELIMITER ;
+
+/* MAIL */
+
+ DROP PROCEDURE IF EXISTS sp_check_usr_mail;
+DELIMITER $$
+	CREATE PROCEDURE sp_check_usr_mail(
+		IN Ihash varchar(64)
+    )
+	BEGIN
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		IF(@id_call>0)THEN        
+			SELECT COUNT(*) AS new_mail FROM tb_mail WHERE id_to = @id_call AND looked=0;
+		ELSE
+			SELECT 0 AS new_mail ;
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_set_mail;
+DELIMITER $$
+	CREATE PROCEDURE sp_set_mail(	
+		IN Ihash varchar(64),
+        IN Iid_to int(11),
+		IN Imessage varchar(512)
+    )
+	BEGIN    
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+        IF(@id_call >0)THEN
+			INSERT INTO tb_mail (id_from,id_to,message) VALUES (@id_call,Iid_to,Imessage);
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_view_mail;
+DELIMITER $$
+	CREATE PROCEDURE sp_view_mail(	
+		IN Ihash varchar(64),
+        IN Isend boolean
+    )
+	BEGIN    
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		IF(@id_call > 0)THEN
+			IF(Isend)THEN
+				SELECT MAIL.*, USR.email AS mail_from
+					FROM tb_mail AS MAIL 
+					INNER JOIN tb_usuario AS USR
+					ON MAIL.id_from = USR.id AND MAIL.id_to = @id_call;            
+            ELSE
+				SELECT MAIL.*, USR.email AS mail_to
+					FROM tb_mail AS MAIL 
+					INNER JOIN tb_usuario AS USR
+					ON MAIL.id_to = USR.id AND MAIL.id_from = @id_call;            
+            END IF;
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_del_mail;
+DELIMITER $$
+	CREATE PROCEDURE sp_del_mail(	
+		IN Ihash varchar(64),
+        IN Idata datetime,
+        IN Iid_from int(11),
+        IN Iid_to int(11)
+    )
+	BEGIN        
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		IF(@id_call = Iid_to OR @id_call = Iid_from)THEN
+			DELETE FROM tb_mail WHERE data = Idata AND id_from = Iid_from AND id_to = Iid_to;
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_mark_mail;
+DELIMITER $$
+	CREATE PROCEDURE sp_mark_mail(	
+		IN Ihash varchar(64),
+        IN Idata datetime,
+        IN Iid_from int(11),
+        IN Iid_to int(11)
+    )
+	BEGIN        
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		IF(@id_call = Iid_to OR @id_call = Iid_from)THEN
+			UPDATE tb_mail SET looked=1 WHERE data = Idata AND id_from = Iid_from AND id_to = Iid_to;
+        END IF;
+	END $$
+DELIMITER ;
+
+ DROP PROCEDURE IF EXISTS sp_all_mail_adress;
+DELIMITER $$
+	CREATE PROCEDURE sp_all_mail_adress(	
+		IN Ihash varchar(64)
+    )
+	BEGIN
+		SET @id_call = (SELECT IFNULL(id,0) FROM tb_usuario WHERE hash COLLATE utf8_general_ci = Ihash COLLATE utf8_general_ci LIMIT 1);
+		SELECT id,email FROM tb_usuario WHERE id != @id_call ORDER BY email ASC;
+	END $$
+DELIMITER ;
+
+/* FIM PADRÂO */
+
+/* BUSCAS E CADASTROS */
+
+ DROP PROCEDURE IF EXISTS sp_view_clube;
+ DROP PROCEDURE IF EXISTS sp_set_clube;
+ DROP PROCEDURE IF EXISTS sp_view_aluno;
+ DROP PROCEDURE IF EXISTS sp_set_aluno;
+ DROP PROCEDURE IF EXISTS sp_view_aula;
+ DROP PROCEDURE IF EXISTS sp_set_aula;
+ DROP PROCEDURE IF EXISTS sp_view_agenda;
+ DROP PROCEDURE IF EXISTS sp_set_agenda;
+ DROP PROCEDURE IF EXISTS sp_view_agenda_dia;
+ DROP PROCEDURE IF EXISTS sp_set_aula_dada;
+ DROP PROCEDURE IF EXISTS sp_view_aula_dada;
+ DROP PROCEDURE IF EXISTS sp_view_fecha_aula;
+ DROP PROCEDURE IF EXISTS sp_view_saldo_devedor;
+ DROP PROCEDURE IF EXISTS sp_set_baixa_aula;
+ DROP PROCEDURE IF EXISTS sp_set_plano;
+ DROP PROCEDURE IF EXISTS sp_add_credit;
+ DROP PROCEDURE IF EXISTS sp_view_credit;
